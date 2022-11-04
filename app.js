89252d710503d7e35417b2af7c4462181c9de0cb
ajay <allcpcontests@gmail.com>
2022-11-04 13:01:26 +0530

    cors added

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
  import {config} from "dotenv";
  import ErrorMiddleware from "./middlewares/Error.js";
  import cookieParser from "cookie-parser";
-
+import cors from "cors";
 config({
     path:"./config/config.env",
 });
@@ -16,6 +16,11 @@ app.use(express.urlencoded({
 }));
 
 app.use(cookieParser());
+app.use(cors({
+    origin:process.env.FRONTEND_URL,
+    credentials:true,
+    methods:["GET","POST","PUT","DELETE"],
+}));
 
 //importing and using routes
 import course from "./routes/courseRoutes.js"
